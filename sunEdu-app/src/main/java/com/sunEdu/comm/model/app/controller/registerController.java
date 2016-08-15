ee7930abe187535b4e127f26dd03015e5fad3171
xufeng <fengxu@mogo-tech.com>
2016-08-15 20:17:03 +0800

    阳光o2o

---
+++ sunEdu-app/src/main/java/com/sunEdu/comm/model/app/controller/registerController.java
@@ -0,0 +1,18 @@
+package com.sunEdu.comm.model.app.controller;/**
+ * Created by Administrator on 2016/8/15 0015.
+ */
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * registerController
+ *
+ * @author xufeng
+ * @date 2016/8/15 0015
+ */
+@RestController
+@RequestMapping(value = "app/api",method = RequestMethod.POST)
+public class registerController {
+}
---
+++ sunEdu-core/src/main/java/com/sunEdu/core/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.sunEdu.core.dao;
+
+import com.sunEdu.comm.model.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
